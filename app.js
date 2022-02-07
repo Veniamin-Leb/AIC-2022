5ec6ac8b7a7584d2258f4c29c9019d59ec4e9ffb
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2022-02-07 18:18:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let raz_block= $("#raz_block");
+let raz = $("#raz");
+let content = $("#content");
+let sver = $("#sver");
+
+raz.on("click", function(event){
+    event.preventDefault();
+    content.addClass("content__input");
+    content.removeClass("hide");
+    raz_block.addClass("hide")
+});
+sver.on("click", function(event){
+    event.preventDefault();
+    content.removeClass("content__input");
+    content.addClass("hide");
+    raz_block.removeClass("hide")
+});
